d3ab5d28d0f2864e609fe7810ae3ec44013c511f
Jose Ariano <143540999+Tannerflea05@users.noreply.github.com>
2025-09-01 11:32:55 -0500

    Añadido ejercicio de explode()

---
+++ TALLERES/TALLER_3/explode.php
@@ -0,0 +1,32 @@
+
+<?php
+// Ejemplo de uso de explode()
+$frase = "Manzana,Naranja,Plátano,Uva";
+$frutas = explode(",", $frase);
+
+echo "Frase original: $frase</br>";
+echo "Array de frutas:</br>";
+print_r($frutas);
+
+// Ejercicio: Crea una variable con una lista de tus 5 películas favoritas separadas por guiones (-)
+// y usa explode() para convertirla en un array
+$misPeliculas = "The batman - Heaven's field - Watering with you - Watchmen - V"; // Reemplaza esto con tu lista de películas
+$arrayPeliculas = explode("-", $misPeliculas);
+
+echo "</br>Mis películas favoritas:</br>";
+print_r($arrayPeliculas);
+
+// Bonus: Usa explode() con un límite
+$texto = "Uno,Dos,Tres,Cuatro,Cinco";
+$array = explode(",", $texto, 3);
+
+echo "</br> Array 2 de peliculas</br>";
+$misPeliculas2 = "The batman - Heaven's field - Watering with you - Watchmen - V"; // Reemplaza esto con tu lista de películas
+$arrayPeliculas2 = explode("-", $misPeliculas2,2);
+print_r($arrayPeliculas2);
+
+echo "</br>Texto original: $texto</br>";
+echo "Array con límite:</br>";
+print_r($array);
+?>
+      
